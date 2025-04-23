75ddd1490eb566b3c0fd9ff6b87567ec98970127
Otabek <53979293+Otabek777@users.noreply.github.com>
2025-04-23 15:51:10 +0500

    slider copy...

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(".asked__block .btn").click(function(){
     $(this).toggleClass("active");
 });
+function copy() {
+    let copyText = document.querySelector(".codecopy");
+    copyText.select();
+    document.execCommand("copy");
+    $(".codecopybtn").addClass("done");
+    setTimeout(function(){
+        $(".codecopybtn").removeClass("done");
+    }, 2000);
+}
+document.querySelector("#copy").addEventListener("click", copy);
