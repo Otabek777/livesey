4783234f16088cd7e3c7f74861bae43073e6b59b
Otabek <53979293+Otabek777@users.noreply.github.com>
2025-04-24 15:59:32 +0500

    walk audio

--- js/main.js
+++ js/main.js
@@ -11,3 +11,20 @@ function copy() {
     }, 2000);
 }
 document.querySelector("#copy").addEventListener("click", copy);
+
+function soundClick() {
+    var audio = document.getElementById('audio');
+    audio.play();
+};
+function soundPauseClick() {
+    var audio = document.getElementById('audio');
+    audio.pause();
+};
+$(".playBtn").click(function() {
+    $(".playBtn").addClass("hide");
+    $(".pauseBtn").removeClass("hide");
+});
+$(".pauseBtn").click(function() {
+    $(".pauseBtn").addClass("hide");
+    $(".playBtn").removeClass("hide");
+});
